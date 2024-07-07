4bde44d516098c0aab8f24c1a522911cbfd8ece1
Md Mahmudur Reza <mahmudreza27@gmail.com>
2024-07-07 19:41:29 -0400

    added search bar at home page

--- ecoswap_app/views.py
+++ ecoswap_app/views.py
@@ -3,9 +3,14 @@ from .models import User, Item, Exchange, Transaction, Message
 from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    items = Item.objects.order_by('-created_at')
-    context ={'items':items}
+    query = request.GET.get('q')
+    if query:
+        items = Item.objects.filter(item_name__icontains=query).order_by('-created_at')
+    else:
+        items = Item.objects.order_by('-created_at')
+    context = {'items': items, 'query': query}
     return render(request, 'ecoswap_app/index.html', context)
+
 
 def item_detail(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
